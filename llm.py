607ad5e0826ad647cb8a1adea7ba260d3e536439
youneedpython <youneedpython@gmail.com>
2025-06-13 15:29:45 +0900

    feat: add few-shot prompting using LangChain FewShotPromptTemplate and example config

--- llm.py
+++ llm.py
@@ -87,9 +87,29 @@ def get_qa_prompt() :
 '''    
     )
 
+
+    ## few-shot ####################################################
+    from langchain_core.prompts import PromptTemplate
+    from langchain_core.prompts import FewShotPromptTemplate
+    from config import answer_examples
+
+    example_prompt = PromptTemplate.from_template("질문: {input}\n\n답변: {answer}")
+
+    few_shot_prompt = FewShotPromptTemplate(
+        examples=answer_examples,           ## 질문/답변 예시들 (전체 type은 list, 각 질문/답변 type은 dict)
+        example_prompt=example_prompt,      ## 단일 예시 포맷
+        prefix='다음 질문에 답변하세요 : ', ## 예시들 위로 추가되는 텍스트(도입부)
+        suffix="Question: {input}",         ## 예시들 뒤에 추가되는 텍스트(실제 사용자 질문 변수)
+        input_variables=["input"],          ## suffix에서 사용할 변수
+    )
+
+    formmated_few_shot_prompt = few_shot_prompt.format(input='{input}')
+    ################################################################
+
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", system_prompt),
+            ('assistant', formmated_few_shot_prompt),
             MessagesPlaceholder("chat_history"),
             ("human", "{input}"),
         ]
